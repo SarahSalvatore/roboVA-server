0be6fa96d168965e47192039b01c1c501c738d65
Sarah Salvatore <sarah.h.salvatore@gmail.com>
2023-01-24 14:29:35 -0500

    Create schema model for users.

---
+++ models/Users.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  roles: [
+    {
+      type: String,
+      default: "Virtual Assistant",
+    },
+  ],
+  active: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
